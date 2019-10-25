9551d4dfa9ea65ffdbbaf8cf03712bc3443f1425
AlaCrity <wlchen1105@gmail.com>
2019-10-25 18:12:52 -0400

    change the folder structure, added and deleted files to get the search results to load

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,45 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import React from "react";
+import Books from "./pages/Books";
 import Nav from "./components/Nav";
 
-import Jumbotron from "./components/Jumbotron";
-
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Nav />
-          <Jumbotron>
-            <h1>Google Books</h1>
-            <h4>Start searcing for your favorite books!</h4>
-          </Jumbotron>
-
-          <Route exact path="/" component={Search} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/saved" component={Saved} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Books />
+    </div>
+  );
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import Nav from "./components/Nav";
+
+// import Jumbotron from "./components/Jumbotron";
+
+// import Search from "./pages/Search";
+// // import Saved from "./pages/Saved";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <Nav />
+//           <Jumbotron>
+//             <h1>Google Books</h1>
+//             <h4>Start searching for your favorite books!</h4>
+//           </Jumbotron>
+
+//           <Route exact path="/" component={Search} />
+//           <Route exact path="/search" component={Search} />
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
--- server.js
+++ server.js
@@ -1,66 +1,68 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-// const routes = require("./routes");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV) {
-  app.use(express.static("client"));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googleBookSearch"
 );
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World");
-});
+app.use(routes);
 
-app.get("/api/books", (req, res) => {
-  db.Book.find({})
-    .then(function(queryResult) {
-      res.json(queryResult);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+// app.get("/hello", (req, res) => {
+//   res.send("Hello World");
+// });
 
-app.post("/api/books", (req, res) => {
-  db.Book.create(req.body)
-    .then(function() {
-      res.send("success");
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+// app.get("/api/books", (req, res) => {
+//   db.Book.find({})
+//     .then(function(queryResult) {
+//       res.json(queryResult);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
-app.delete("/api/books/:id", (req, res) => {
-  db.Book.find({ _id: req.params.id })
-    .remove()
-    .then(function() {
-      res.send("Comment deleted.");
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+// app.post("/api/books", (req, res) => {
+//   db.Book.create(req.body)
+//     .then(function() {
+//       res.send("success");
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
+
+// app.delete("/api/books/:id", (req, res) => {
+//   db.Book.find({ _id: req.params.id })
+//     .remove()
+//     .then(function() {
+//       res.send("Comment deleted.");
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "index.html"));
+// });
 
 // Start the API server
 app.listen(PORT, function() {
